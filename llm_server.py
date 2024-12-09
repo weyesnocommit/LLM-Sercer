8444bcb8db150853d6cd0a1330e83d29a5e0d77b
iyesbusiness <87265735+weyesnocommit@users.noreply.github.com>
2024-12-09 18:46:02 +0100

    custom TOKEN AH

--- llm_server.py
+++ llm_server.py
@@ -3,13 +3,14 @@ torch.set_num_threads(4)
 import zmq
 import msgpack
 from fastT5 import get_onnx_model
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, LongT5ForConditionalGeneration
 from config import LLM_SERCER_PORT
 from post_processors import POST_PORCESSORS
 import json
 import time
 import logging
 import os
+import traceback
 
 logging.basicConfig(
     level=logging.INFO,
@@ -26,6 +27,7 @@ class TextProcessorServer:
         self.run_count = 0
         self.models = {}
         self.load_fastt5_models("./models/fastt5")
+        self.load_torch_models("./models/torch")
 
     def load_fastt5_models(self, directory):
         """Dynamically load all models and tokenizers in the given directory."""
@@ -40,7 +42,33 @@ class TextProcessorServer:
                         'tokenizer': tokenizer,
                         'generator': self.gen_t5
                     }
+                    special_tokens = {
+                        "additional_special_tokens": [
+                            "</msg>", "</att_pic>", "</att_aud>", "</att_fil>", "</uid>", "</name>"
+                        ]
+                    }
+                    # Add the special tokens to the tokenizer
+                    tokenizer.add_special_tokens(special_tokens)
                     logger.info(f"loddddddddddddddddddddddd '{model_name}' from '{model_path}'")
+                except Exception as e:
+                    logger.error(f"NOTT! '{model_name}' at '{model_path}': {e}")
+                    
+    def load_torch_models(self, directory):
+        """Dynamically load all models and tokenizers in the given directory."""
+        for model_name in os.listdir(directory):
+            model_path = os.path.join(directory, model_name)
+            if os.path.isdir(model_path):
+                try:
+                    model = LongT5ForConditionalGeneration.from_pretrained(model_path)
+                    tokenizer = AutoTokenizer.from_pretrained(model_path)
+                    self.models[model_name] = {
+                        'model': model,
+                        'tokenizer': tokenizer,
+                        'generator': self.gen_t5
+                    }
+                    logger.info(f"loddddddddddddddddddddddd '{model_name}' from '{model_path}'")
+                    print("Model class:", type(model).__name__)
+                    print("Tokenizer class:", type(tokenizer).__name__)
                 except Exception as e:
                     logger.error(f"NOTT! '{model_name}' at '{model_path}': {e}")
 
@@ -50,12 +78,17 @@ class TextProcessorServer:
             return "NOTTT"
         
         start_time = time.perf_counter()
-        tokenized_input = tokenizer(txt, return_tensors='pt')
-
+        tokenized_input = tokenizer(
+            txt, 
+            return_tensors='pt', 
+            padding=False,
+            truncation=True,  # Enables truncation
+            max_length=512    # Specify the maximum length
+        )
         tokens = model.generate(
             input_ids=tokenized_input['input_ids'],
             attention_mask=tokenized_input['attention_mask'],
-            num_beams=config.get('num_beams', 2),
+            num_beams=config.get('num_beams', 1),
             temperature=config.get('temperature', 1),
             do_sample=config.get('do_sample', True),
             max_new_tokens=config.get('max_new_tokens', 500),
@@ -87,6 +120,7 @@ class TextProcessorServer:
             out = self.models[model_name]['generator'](model, tokenizer, config, text)
             if model_name in POST_PORCESSORS:
                 return POST_PORCESSORS[model_name](out)
+            logger.info(f"generation: {out}")
             return out
         else:
             model = self.models[DEFAULT_MODEL]['model']
@@ -121,12 +155,13 @@ class TextProcessorServer:
                     
                 response = self.gen(data)
                 if msg_type == 0:
-                    packed_response = msgpack.packb(response)
+                    packed_response = msgpack.packb(response)#, flags=zmq.NOBLOCK)
                     self.socket.send(packed_response)
                 else:
                     packed_response = json.dumps(response)
-                    self.socket.send_string(packed_response)
+                    self.socket.send_string(packed_response)#, flags=zmq.NOBLOCK)
             except Exception as e:
+                logger.error(traceback.format_exc())
                 if "current state" in str(e):
                     self.socket.close()
                     self.socket = self.context.socket(zmq.REP)
